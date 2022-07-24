d00919c5784a55bc54bb6f4126bfff9c0b4c87db
Vago Aleksandr <vgoalex666@mail.ru>
2022-07-25 02:15:46 +0300

    feat: completed push in heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const router = jsonServer.router('./db.json');
 const middlewares = jsonServer.defaults({
   static: './build',
 });
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 server.use(middlewares);
 server.use(
   jsonServer.rewriter({
