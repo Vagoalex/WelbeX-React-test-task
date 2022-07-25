3c044237700bb226686b3db6a47e44d34a82d6e4
Vago Aleksandr <vgoalex666@mail.ru>
2022-07-25 04:08:45 +0300

    refactor: refactored filters

--- src/helpers/sortData.js
+++ src/helpers/sortData.js
@@ -6,10 +6,6 @@ function sortData(a, b, activeFilter) {
       if (nameA < nameB) return -1;
       if (nameA > nameB) return 1;
       return 0;
-    case 'quantity':
-      return a[activeFilter] - b[activeFilter];
-    case 'distance':
-      return b[activeFilter] - a[activeFilter];
     default:
       throw new Error('Invalid active filter');
   }
